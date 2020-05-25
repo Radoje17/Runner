217a6a0a4d681ee563f9168590febbffdf75632a
Radoje17 <radoje707@gmail.com>
2020-05-25 14:42:56 +0200

    Initial commit

---
+++ src/main/java/me/radoje17/runner/Runner.java
@@ -0,0 +1,13 @@
+package me.radoje17.runner;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Runner extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
